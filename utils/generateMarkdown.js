b82699b2d2991624012a879a68022d521774cdb7
Richard Murphy <richinatl@gmail.com>
2022-03-14 21:11:22 -0400

    adding remainder of functions before testing

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -33,7 +33,7 @@ function renderLicenseSection(license) {
     return ``;
   } else {
     return `## Licenses
-    This project is covered under the ${license} license. To learn more about what this means, click the license button at the top.`;
+    This project is covered under the ${license} license. To learn more click the license button at the top.`;
   }
 }
 
